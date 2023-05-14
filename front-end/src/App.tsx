7535db6f0651bb1ad61b85219d0722d6669d37cb
emerson gonçalves <emer_gs@hotmail.com>
2023-05-13 23:06:16 -0300

    feat:initial construction of the front end layout

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  const validatedFile = () => {
+    console.log(file)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <header>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={validatedFile}>Validar </button>
+      </header>
+      <section>
+
+      </section>
+    </main>
   )
 }
 
